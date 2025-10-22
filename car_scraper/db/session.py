1a118cf125f47122f89a191c6746ece103169dcd
Jackson Lima <jacksonlima2347@gmail.com>
2025-10-22 17:51:34 -0300

    feat: refactor brand service and scraping logic with Playwright

---
+++ car_scraper/db/session.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from car_scraper.utils.config import settings
+
+#engine = create_engine(settings.DATABASE_URL, pool_size=5, max_overflow=5, future=True,echo=True)
+engine = create_engine(settings.DATABASE_URL, pool_size=5, max_overflow=5, future=True)
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False, autoflush=False)
--- car_scraper/utils/human.py
+++ car_scraper/utils/human.py
@@ -1,5 +1,6 @@
-import time, random
-
+import time
+import random
+from bs4 import BeautifulSoup
 
 def human_delay(a=0.8, b=2.0):
     time.sleep(random.uniform(a, b))
@@ -12,3 +13,27 @@ def human_scroll(page, distance=600):
         page.evaluate("window.scrollBy(0, %s)" % step)
         scrolled += step
         time.sleep(random.uniform(0.05, 0.18))
+
+
+def human_scroll_to_bottom(page, step=400):
+    last_height = page.evaluate("document.body.scrollHeight")
+
+    while True:
+        page.evaluate(f"window.scrollBy(0, {step})")
+        time.sleep(random.uniform(0.1, 0.3))  # pausa entre os scrolls
+        time.sleep(random.uniform(0.5, 1.0))
+        new_height = page.evaluate("document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+def show_html(container, size=2000):
+    html_content = container.inner_html()
+    soup = BeautifulSoup(html_content, "html.parser")
+    pretty_html = soup.prettify()
+
+    print("===== CONTAINER HTML =====")
+    print(pretty_html[:size])
+    print("===== END OF HTML =====")
